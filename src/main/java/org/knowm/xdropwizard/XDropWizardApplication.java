ff5380597bbeb1c63b5d8d81d29e694abe68c7e8
suamorales <morales.sua@gmail.com>
2016-08-21 07:51:14 -0400

    Clean up.

--- src/main/java/org/knowm/xdropwizard/XDropWizardApplication.java
+++ src/main/java/org/knowm/xdropwizard/XDropWizardApplication.java
@@ -16,18 +16,8 @@
  */
 package org.knowm.xdropwizard;
 
-import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
-import org.knowm.dropwizard.sundial.SundialBundle;
-import org.knowm.dropwizard.sundial.SundialConfiguration;
-import org.knowm.xdropwizard.business.User;
 import org.knowm.xdropwizard.health.TemplateHealthCheck;
-import org.knowm.xdropwizard.manager.YankManager;
-import org.knowm.xdropwizard.resources.AuthenticatorExample;
-import org.knowm.xdropwizard.resources.HelloWorldResource;
 import org.knowm.xdropwizard.resources.WeatherResource;
-import org.knowm.xdropwizard.resources.YankBookResource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import io.dropwizard.Application;
 import io.dropwizard.assets.AssetsBundle;
@@ -44,8 +34,6 @@ import io.dropwizard.views.ViewBundle;
  */
 public class XDropWizardApplication extends Application<XDropWizardApplicationConfiguration> {
 
-  private final Logger logger = LoggerFactory.getLogger(XDropWizardApplication.class);
-
   public static void main(String[] args) throws Exception {
 
     new XDropWizardApplication().run(args);
@@ -57,40 +45,20 @@ public class XDropWizardApplication extends Application<XDropWizardApplicationCo
     bootstrap.addBundle(new AssetsBundle("/assets/", "/"));
     bootstrap.addBundle(new ViewBundle<XDropWizardApplicationConfiguration>());
 
-    bootstrap.addBundle(new SundialBundle<XDropWizardApplicationConfiguration>() {
-
-      @Override
-      public SundialConfiguration getSundialConfiguration(XDropWizardApplicationConfiguration configuration) {
-        return configuration.getSundialConfiguration();
-      }
-    });
-
-    // for use with `dropwizard-forms`
-    bootstrap.addBundle(new MultiPartBundle());
-  }
+    }
 
   @Override
   public void run(XDropWizardApplicationConfiguration configuration, Environment environment) throws Exception {
 
-    logger.info("running DropWizard!");
 
     // Add object to ServletContext for accessing from Sundial Jobs
     environment.getApplicationContext().setAttribute("MyKey", "MyObject");
 
     final String template = configuration.getTemplate();
     final String defaultName = configuration.getDefaultName();
-    environment.jersey().register(new HelloWorldResource(template, defaultName));
-
     environment.jersey().register(new WeatherResource("San Fran", 32, 0));
-
     environment.healthChecks().register("TemplateHealth", new TemplateHealthCheck(template));
 
-    // MANAGERS /////////////////////////
-
-    // Yank
-    YankManager ym = new YankManager(configuration.getYankConfiguration()); // A DropWizard Managed Object
-    environment.lifecycle().manage(ym); // Assign the management of the object to the Service
-    environment.jersey().register(new YankBookResource());
 
     // TASKS ////////////////////////////
     // tasks are things that should run triggered by a POST, but don't need to respond
@@ -99,19 +67,9 @@ public class XDropWizardApplication extends Application<XDropWizardApplicationCo
     // RESOURCES ////////////////////////////
 
     //    environment.jersey().register(new XChartResource());
-    //    environment.jersey().register(new ViewBookResource());
     //    environment.jersey().register(new ViewMarkdownResource());
-    //    environment.jersey().register(new RandomNumberResource());
 
     environment.jersey().packages("org.knowm.xdropwizard.resources");
-
-    // AUTHENTICATION /////////////////////////
-
-    environment.jersey().register(new AuthDynamicFeature(
-        new BasicCredentialAuthFilter.Builder<User>().setAuthenticator(new AuthenticatorExample()).setRealm("SUPER SECRET STUFF").buildAuthFilter()));
-    environment.jersey().register(RolesAllowedDynamicFeature.class);
-    //If you want to use @Auth to inject a custom Principal type into your resource
-    environment.jersey().register(new AuthValueFactoryProvider.Binder<>(User.class));
-
   }
-}
+ }
+
